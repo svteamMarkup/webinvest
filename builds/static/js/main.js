fec33e82f851b92a4fc724f423b421495caac64a
extiser <1sappho@gmail.com>
2017-11-27 10:24:20 +0500

    rebuild

---
+++ builds/static/js/main.js
@@ -0,0 +1,27 @@
+
+
+
+
+$(document).ready(function() {
+  $(".header-menu__item").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    $(this).addClass('_active').siblings().removeClass('_active');
+    $("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination - 50
+    }, 800);
+    return false;
+  });
+
+  $(window).on('scroll', function () {
+    if ($(this).scrollTop() > 50) {
+      $('.header').addClass('_active');
+    } else {
+      $('.header').removeClass('_active');
+    }
+  });
+
+});
+
+
+
